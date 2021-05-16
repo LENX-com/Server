38ec4df732d3d5c3438ec87264685f7324c03943
LENX-com <80533622+LENX-com@users.noreply.github.com>
2021-05-17 01:55:08 +0400

    Add files via upload

---
+++ utils/channels.js
@@ -0,0 +1,13 @@
+let STATIC_CHANNELS = [{
+    name: 'Global chat',
+    participants: 0,
+    id: 1,
+    sockets: []
+}, {
+    name: 'LENX Support',
+    participants: 0,
+    id: 2,
+    sockets: []
+}];
+
+module.exports = { STATIC_CHANNELS }
