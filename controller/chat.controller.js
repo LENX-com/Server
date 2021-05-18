a651c2b8cf5e45094eab921d19e7c7b4a2c2f0f5
addegbenga <addegbenga@gmail.com>
2021-05-18 07:47:35 +0100

    changes

--- controller/chat.controller.js
+++
@@ -1,7 +0,0 @@
-let { STATIC_CHANNELS } = require('../utils/channels');
-
-exports.getChannels = (req, res) => {
-    res.json({
-        channels: STATIC_CHANNELS
-    });
-}
--- routes/chat.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-let { getChannels } = require('../controller/chat.controller');
-
-router.get('/getChannels', getChannels);
-
-
-module.exports = router;
